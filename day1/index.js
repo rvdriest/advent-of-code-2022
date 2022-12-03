4edaf0330a406327a199b3ca4005dcb43b07d5ec
Richard van Driest <richard.vandriest@outlook.com>
2022-12-03 01:54:33 +0100

    remove log

--- day1/index.js
+++ day1/index.js
@@ -8,13 +8,6 @@ fs.readFile("input.txt", "utf-8", (err, data) => {
     amount: calculateElveAmount(elves[0]),
   };
 
-  console.log(
-    elves[0]
-      .split("\n")
-      .map((n) => +n)
-      .reduce((acc, cur) => acc + cur)
-  );
-
   for (let i = 1; i < elves.length; i++) {
     const elveAmount = calculateElveAmount(elves[i]);
     if (elveAmount >= elveWithMostFood.amount) {
