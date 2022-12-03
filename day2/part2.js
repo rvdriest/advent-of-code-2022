2e8d66689ca2f0f644c467d4777e07169748639e
Richard van Driest <richard.vandriest@outlook.com>
2022-12-03 02:21:43 +0100

    Part 2 of day 2 done

---
+++ day2/part2.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", "utf-8", (err, data) => {
+  const rounds = data.split(/\n/);
+  let totalScore = 0;
+
+  for (let i = 0; i < rounds.length; i++) {
+    const [opponentValue, outcome] = rounds[i].split(" ");
+    totalScore += calculateScore(opponentValue, outcome);
+  }
+  console.log(totalScore);
+});
+
+const valueMap = new Map();
+valueMap.set("Rock", 1);
+valueMap.set("Paper", 2);
+valueMap.set("Scissor", 3);
+
+const losingValues = new Map();
+losingValues.set("A", "Scissor");
+losingValues.set("B", "Rock");
+losingValues.set("C", "Paper");
+
+const drawingValues = new Map();
+drawingValues.set("A", "Rock");
+drawingValues.set("B", "Paper");
+drawingValues.set("C", "Scissor");
+
+const winningValues = new Map();
+winningValues.set("A", "Paper");
+winningValues.set("B", "Scissor");
+winningValues.set("C", "Rock");
+
+/**
+ * Rock (A, X) + 1
+ * Paper (B, Y) + 2
+ * Scissors (C, Z) + 3
+ *
+ * Win + 6
+ * Draw + 3
+ * Lose + 0
+ * @param {*} opponentValue
+ * @param {*} myValue
+ * @returns score of round
+ */
+function calculateScore(opponentValue, outcome) {
+  // Lose the game
+  if (outcome === "X") {
+    return valueMap.get(losingValues.get(opponentValue));
+  } else if (outcome === "Y") {
+    return 3 + valueMap.get(drawingValues.get(opponentValue));
+  }
+
+  return 6 + valueMap.get(winningValues.get(opponentValue));
+}
