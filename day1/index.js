3f2154a3de913c4efd599f59a4d21c589e80c2b1
Richard van Driest <richard.vandriest@outlook.com>
2022-12-03 01:05:45 +0100

    Day 1 done

---
+++ day1/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", "utf-8", (err, data) => {
+  const elves = data.split("\n\n");
+
+  let elveWithMostFood = {
+    index: 0,
+    amount: calculateElveAmount(elves[0]),
+  };
+
+  console.log(
+    elves[0]
+      .split("\n")
+      .map((n) => +n)
+      .reduce((acc, cur) => acc + cur)
+  );
+
+  for (let i = 1; i < elves.length; i++) {
+    const elveAmount = calculateElveAmount(elves[i]);
+    if (elveAmount >= elveWithMostFood.amount) {
+      elveWithMostFood = {
+        index: i,
+        amount: elveAmount,
+      };
+    }
+  }
+  console.log(elveWithMostFood);
+});
+
+function calculateElveAmount(elve) {
+  return elve
+    .split("\n")
+    .map((n) => +n)
+    .reduce((acc, cur) => acc + cur);
+}
